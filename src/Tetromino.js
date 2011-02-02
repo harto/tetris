6117cf73d233fb5f3b3e72dedd3a406fc6dc94fd
Stuart Campbell <stuart@harto.org>
2011-02-03 09:26:01 +1100

    experimenting with rotation

--- src/Tetromino.js
+++ src/Tetromino.js
@@ -9,11 +9,13 @@ T.Tetromino = function (shape) {
     this.shape = shape;
 
     var colour, layout;
+
     switch (shape) {
     case 'I':
         layout = [
             '####'
         ];
+        this.offsets = [];//[[-1, 0], [0, -1], [1, 0], [0, 1]];
         colour = '#F00'; // red
         break;
     case 'J':
@@ -21,6 +23,7 @@ T.Tetromino = function (shape) {
             '###',
             '  #'
         ];
+        this.offsets = [];
         colour = '#FF0'; // yellow
         break;
     case 'L':
@@ -28,6 +31,7 @@ T.Tetromino = function (shape) {
             '###',
             '#  '
         ];
+        //this.offsets = [[-1, 0], [-1, 0], [-1, 0], ];
         colour = '#F0F'; // magenta
         break;
     case 'O':
@@ -35,6 +39,7 @@ T.Tetromino = function (shape) {
             '##',
             '##'
         ];
+        this.offsets = [];
         colour = '#00F'; // blue
         break;
     case 'S':
@@ -42,6 +47,7 @@ T.Tetromino = function (shape) {
             ' ##',
             '## '
         ];
+        this.offsets = [[1, 0], [-1, -1]];
         colour = '#0FF'; // cyan
         break;
     case 'T':
@@ -49,6 +55,7 @@ T.Tetromino = function (shape) {
             '###',
             ' # '
         ];
+        this.offsets = [[1, -1], [-1, -1], [0, 0], [0, 0]];
         colour = '#0F0'; // green
         break;
     case 'Z':
@@ -56,6 +63,7 @@ T.Tetromino = function (shape) {
             '## ',
             ' ##'
         ];
+        this.offsets = [[1, 0], [-1, -1]];
         colour = '#F90'; // orange
         break;
     default:
@@ -77,38 +85,63 @@ T.Tetromino = function (shape) {
 };
 
 T.Tetromino.prototype = {
-    
+
     render: function (ctx) {
         ctx.save();
         ctx.translate(this.x * T.CELL_W, this.y * T.CELL_H);
         
-        if (T.debug) {
-            ctx.strokeStyle = '#000';
-            ctx.strokeRect(0, 0, this.w * T.CELL_W, this.h * T.CELL_H);
-        }
-        
         this.tiles.forEach(function (tile) {
             tile.render(ctx);
         });
+
+        if (T.debug) {
+            // bounding box
+            ctx.strokeStyle = '#000';
+            ctx.strokeRect(0, 0, this.w * T.CELL_W, this.h * T.CELL_H);
+        }
+                
         ctx.restore();
     },
 
     rotate: function () {
-        var axis = Vector.create([this.w / 2, this.h / 2]);
+        var offset = this.offsets.shift();
+        if (offset) {
+            // re-queue
+            this.offsets.push(offset);
+        }
+        else {
+            offset = [0, 0];
+        }
+
+        var angle = 3 * Math.PI / 2;
+        var axis = Vector.create([Math.floor(this.w / 2), Math.floor(this.h / 2)]);
+
         this.tiles.forEach(function (tile) {
             var v = Vector.create([tile.x, tile.y]);
-            v = v.rotate(-Math.PI / 2, axis);
-            tile.x = v.e(1);//Math.floor(v.e(1));
-            tile.y = v.e(2);//Math.floor(v.e(2));
+            v = v.rotate(angle, axis).round();
+            tile.x = v.e(1) + offset[0];
+            tile.y = v.e(2) + offset[1];
         });
 
-        // update dimensions
-        this.w = Math.max.apply(null, this.tiles.map(function (tile) {
-            return tile.x;
-        })) + 1;
-        this.h = Math.max.apply(null, this.tiles.map(function (tile) {
-            return tile.y;
-        })) + 1;
+        var xs = this.tiles.map(function (tile) { return tile.x; });
+        var ys = this.tiles.map(function (tile) { return tile.y; });
+
+        var minX = Math.min.apply(null, xs), maxX = Math.max.apply(null, xs),
+            minY = Math.min.apply(null, ys), maxY = Math.max.apply(null, ys);
+
+        console.log('minx=' + minX + ', miny=' + minY + ', maxX=' + maxX + ', maxY=' + maxY);
+
+        // recalculate dimensions
+        this.w = maxX - minX + 1;
+        this.h = maxY - minY + 1;
+
+        // relocate tiles within frame
+        this.tiles.forEach(function (tile) {
+            tile.x -= minX;
+            tile.y -= minY;
+        });
+        this.x += minX;
+        this.y += minY;
     },
 
     collidesWith: function (tile) {
